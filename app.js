e2dd791976f61d98e35960ea6d1aef2d3ba615e1
edu23ard <e.2.d.3.u.ard@gmail.com>
2025-02-02 16:33:51 -0600

    creacion de array para el almacenamiento de los nombres

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+// Crea una variable de tipo array, que almacenará los nombres de los amigos ingresados. 
+let listaAmigos = [];
+
+
+
+
