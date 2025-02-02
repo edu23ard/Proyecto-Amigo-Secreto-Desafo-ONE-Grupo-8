6a926aed9c7bcd7e14e50cc989e382d0041d4acd
edu23ard <e.2.d.3.u.ard@gmail.com>
2025-02-02 16:51:09 -0600

    Se agrego la funcion para agregar amigos

--- app.js
+++ app.js
@@ -4,6 +4,36 @@
 // Crea una variable de tipo array, que almacenará los nombres de los amigos ingresados. 
 let listaAmigos = [];
 
+// funcion captura el nombre de los amigos en una array
+function agregarAmigo() {
+    let amigosList = document.getElementById("amigo").value;
+    //valida el input
+    if (amigosList === "") {
+        alert("Por favor, inserte un nombre");
+        return;
+    }
+
+    //verifica si el amigo ya esta en la array
+    if (listaAmigos.includes(amigosList)) {
+        alert("El amigo ya se encuentra en la lista");
+        return; 
+    }
+
+    //actualiza la array de amigos
+    listaAmigos.push(amigosList);
+
+    //llama a la funcion limpia el input
+    limpiarInput();
+           
+}
+
+//funcion limpia el input
+ function limpiarInput() {
+        document.querySelector("#amigo").value = "";
+    }
+
+
+
 
 
 
